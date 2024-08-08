53c9c5912c406bee9536c0a6030fb89bbfe656f7
Leo Lobis <leo.lobis02@gmail.com>
2024-08-08 22:05:24 +0200

    Initial commit

---
+++ todo-app-backed/src/main/java/com/github/leloxo/todoapp/TodoAppBackedApplication.java
@@ -0,0 +1,13 @@
+package com.github.leloxo.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppBackedApplication.class, args);
+	}
+
+}
---
+++ todo-app-backed/src/test/java/com/github/leloxo/todoapp/TodoAppBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leloxo.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
