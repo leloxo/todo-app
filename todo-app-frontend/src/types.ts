a9509981c4df5af17a15e79390b969b48a7b7475
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 15:00:05 +0200

    Add types.ts - Define Todo type

---
+++ todo-app-frontend/src/types.ts
@@ -0,0 +1,12 @@
+export interface Todo {
+    id: number;
+    title: string;
+    description: string;
+    creationDate: string;
+    dueDate: string;
+    priority: string;
+    status: string;
+    category: string;
+    completionDate: string;
+    tags: string[];
+}
