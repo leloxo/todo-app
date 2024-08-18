8bfa213530602de4a83bf4ca4541a225f09903d9
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 18:06:56 +0200

    Update TodoItem.tsx - Add styling from TodoItem.css

--- todo-app-frontend/src/components/TodoItem.tsx
+++ todo-app-frontend/src/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../types';
+import './TodoItem.css';
 
 interface TodoItemProps {
     todo: Todo;
@@ -8,17 +9,18 @@ interface TodoItemProps {
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete }) => {
     return (
-        <div>
-            <h2>{todo.title}</h2>
-            <p>{todo.description}</p>
-            <p>Creation Date: {todo.creationDate}</p>
-            <p>Due Date: {todo.dueDate}</p>
-            <p>Priority: {todo.priority}</p>
-            <p>Status: {todo.status}</p>
-            <p>Category: {todo.category}</p>
-            <p>completion Date: {todo.completionDate}</p>
-            <p>Tags: {todo.tags}</p>
-            <button onClick={() => onDelete(todo.id)}>Delete Todo</button>
+        <div className='todo-item-container'>
+            <div className='todo-item'>
+                <p className='todo-title'>{todo.title}</p>
+                <p>{todo.description}</p>
+                <p>Creation Date: {todo.creationDate} Due Date: {todo.dueDate}</p>
+                <p>Priority: {todo.priority}</p>
+                <p>Status: {todo.status}</p>
+                <p>Category: {todo.category}</p>
+                <p>completion Date: {todo.completionDate}</p>
+                <p>Tags: {todo.tags}</p>
+                <button onClick={() => onDelete(todo.id)}>Delete Todo</button>
+            </div>
         </div>
     );
 };
