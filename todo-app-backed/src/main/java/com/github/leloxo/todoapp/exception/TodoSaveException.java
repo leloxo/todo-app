eafff7ff1f7fd0da85b2aa7783011d70a9ed5e03
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 11:00:29 +0200

    Add TodoSaveException class

---
+++ todo-app-backed/src/main/java/com/github/leloxo/todoapp/exception/TodoSaveException.java
@@ -0,0 +1,7 @@
+package com.github.leloxo.todoapp.exception;
+
+public class TodoSaveException extends RuntimeException {
+    public TodoSaveException(String message) {
+        super(message);
+    }
+}
