c83884d76125d3cb7ab2fa1bc69893bfb672a032
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 16:29:06 +0200

    Update App.tsx - Display TodoList component

--- todo-app-frontend/src/App.tsx
+++ todo-app-frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
     return (
-        <div>Hello World</div>
+        <div>
+            <TodoList />
+        </div>
     );
 };
 
