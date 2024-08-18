d68dd16c0098c160ec8e84d802dafe0a29bf1f7f
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 18:05:11 +0200

    Update App.tsx - Add title

--- todo-app-frontend/src/App.tsx
+++ todo-app-frontend/src/App.tsx
@@ -5,6 +5,7 @@ import TodoList from './components/TodoList';
 const App: React.FC = () => {
     return (
         <div>
+            <h1>TODO LIST</h1>
             <TodoList />
         </div>
     );
