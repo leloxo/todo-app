55907242e670d2c9278f99f1d1ba7a7ccaec52f3
Leo Lobis <leo.lobis02@gmail.com>
2024-08-18 11:05:12 +0200

    Add TodoController class

---
+++ todo-app-backed/src/main/java/com/github/leloxo/todoapp/controller/TodoController.java
@@ -0,0 +1,49 @@
+package com.github.leloxo.todoapp.controller;
+
+import com.github.leloxo.todoapp.model.Todo;
+import com.github.leloxo.todoapp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        List<Todo> todos = todoService.getAllTodos();
+        return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        Todo todo = todoService.getTodoById(id);
+        return ResponseEntity.ok(todo);
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+        Todo createdTodo = todoService.saveTodo(todo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTodo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        todo.setId(id);
+        Todo updatedTodo = todoService.saveTodo(todo);
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
