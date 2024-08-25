67feb66091564084778b0116c7dc8d962a734658
Leo Lobis <leo.lobis02@gmail.com>
2024-08-25 11:15:26 +0200

    Add styles to App.tsx

--- todo-app-frontend/src/App.tsx
+++ todo-app-frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodoList from './components/TodoList/TodoList';
+import styles from './app.module.scss'
 
 const App: React.FC = () => {
     return (
         <div>
-            <h1>TODO LIST</h1>
-            <TodoList />
+            <h1 style={{ textAlign: 'center'}}>TODO LIST</h1>
+            <div className={styles.todoItemContainer}>
+                <TodoList />
+            </div>
         </div>
     );
 };
