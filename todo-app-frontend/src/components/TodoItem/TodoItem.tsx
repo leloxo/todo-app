81c830fe0c341e51bff083f884d8ff219241219e
Leo Lobis <leo.lobis02@gmail.com>
2024-08-25 11:22:54 +0200

    Update styling in TodoItem.tsx

--- todo-app-frontend/src/components/TodoItem/TodoItem.tsx
+++ todo-app-frontend/src/components/TodoItem/TodoItem.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Todo } from '../../types/types';
-import styles from './TodoItem.module.scss';
+import styles from './todoItem.module.scss';
 
 interface TodoItemProps {
     todo: Todo;
     onDelete: (id: number) => void;
+    onEdit: (todo: Todo) => void;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete }) => {
+const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete, onEdit }) => {
     return (
-        <div className={styles.todoItemContainer}>
+        <div>
             <div className={styles.todoItem}>
-                <p>{todo.title}</p>
-                <p>{todo.description}</p>
-                <p>Creation Date: {todo.creationDate} Due Date: {todo.dueDate}</p>
-                <p>Priority: {todo.priority}</p>
-                <p>Status: {todo.status}</p>
-                <p>Category: {todo.category}</p>
-                <p>completion Date: {todo.completionDate}</p>
-                <p>Tags: {todo.tags}</p>
-                <button onClick={() => onDelete(todo.id)}>Delete Todo</button>
+                <p className={styles.title}>{todo.title}</p>
+                <div className={styles.gridContainer}>
+                    <p>Creation Date: {todo.creationDate}</p>
+                    <p>Due Date: {todo.dueDate}</p>
+                    <p>Priority: {todo.priority}</p>
+                    <p>Status: {todo.status}</p>
+                </div>
+                {/* <section>{todo.description}</section> */}
+                {/* <p>Category: {todo.category}</p> */}
+                {/* <p>completion Date: {todo.completionDate}</p> */}
+                {/* <p>Tags: {todo.tags}</p> */}
+
+                <div className={styles.buttonContainer}>
+                    <button onClick={() => onEdit(todo)}>Edit</button>
+                    <button onClick={() => onDelete(todo.id)}>Delete</button>
+                </div>
             </div>
         </div>
     );
