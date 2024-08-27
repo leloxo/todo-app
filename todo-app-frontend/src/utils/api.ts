18aa931747867236d2615558400086ab6107af6a
Leo Lobis <leo.lobis02@gmail.com>
2024-08-27 22:03:34 +0200

    Add additional errorhandling in api.ts

--- todo-app-frontend/src/utils/api.ts
+++ todo-app-frontend/src/utils/api.ts
@@ -19,20 +19,60 @@ export const getAllTodos = async (): Promise<Todo[]> => {
 };
 
 export const getTodoById = async (id: number): Promise<Todo> => {
-    const response = await axios.get(`${API_URL}/${id}`);
-    return response.data;
+    try {
+        const response = await axios.get(`${API_URL}/${id}`);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error:', error.message);
+            throw new Error(`Error: ${error.message}`);
+        } else {
+            console.error('Unexpected error:', error);
+            throw new Error('An unexpected error occurred.');
+        }
+    }
 };
 
 export const createTodo = async (todo: Omit<Todo, 'id'>): Promise<Todo> => {
-    const response = await axios.post(API_URL, todo);
-    return response.data;  
+    try {
+        const response = await axios.post(API_URL, todo);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error:', error.message);
+            throw new Error(`Error: ${error.message}`);
+        } else {
+            console.error('Unexpected error:', error);
+            throw new Error('An unexpected error occurred.');
+        }
+    }
 };
 
 export const updateTodo = async (id: number, todo: Todo): Promise<Todo> => {
-    const response = await axios.put(`${API_URL}/${id}`, todo);
-    return response.data;
+    try {
+        const response = await axios.put(`${API_URL}/${id}`, todo);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error:', error.message);
+            throw new Error(`Error: ${error.message}`);
+        } else {
+            console.error('Unexpected error:', error);
+            throw new Error('An unexpected error occurred.');
+        }
+    }
 };
 
 export const deleteTodo = async (id: number): Promise<void> => {
-    axios.delete(`${API_URL}/${id}`);
+    try {
+        await axios.delete(`${API_URL}/${id}`);
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error('Error:', error.message);
+            throw new Error(`Error: ${error.message}`);
+        } else {
+            console.error('Unexpected error:', error);
+            throw new Error('An unexpected error occurred.');
+        }
+    }
 };
