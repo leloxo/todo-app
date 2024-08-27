7a17a7814571006f3a9956c8ca3565874a7bd741
Leo Lobis <leo.lobis02@gmail.com>
2024-08-27 22:05:55 +0200

    Add DEFAULT value to Status and Priority enum

--- todo-app-backed/src/main/java/com/github/leloxo/todoapp/model/Priority.java
+++ todo-app-backed/src/main/java/com/github/leloxo/todoapp/model/Priority.java
@@ -3,5 +3,6 @@ package com.github.leloxo.todoapp.model;
 public enum Priority {
     LOW,
     MEDIUM,
-    HIGH
+    HIGH,
+    DEFAULT
 }
--- todo-app-backed/src/main/java/com/github/leloxo/todoapp/model/Status.java
+++ todo-app-backed/src/main/java/com/github/leloxo/todoapp/model/Status.java
@@ -3,5 +3,6 @@ package com.github.leloxo.todoapp.model;
 public enum Status {
     TODO,
     IN_PROGRESS,
-    DONE
+    DONE,
+    DEFAULT
 }
