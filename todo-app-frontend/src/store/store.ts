572827cca263f970868c3ea0bfe58932640f8dea
Leo Lobis <leo.lobis02@gmail.com>
2024-08-27 22:06:52 +0200

    Update store.ts to use devTools

--- todo-app-frontend/src/store/store.ts
+++ todo-app-frontend/src/store/store.ts
@@ -5,6 +5,7 @@ export const store = configureStore({
   reducer: {
     todo: todoReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
