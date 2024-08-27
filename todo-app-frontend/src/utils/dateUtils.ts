fa56a2ff54dd835503076a55821564acaa3cbdf6
Leo Lobis <leo.lobis02@gmail.com>
2024-08-27 22:04:29 +0200

    Add dateUtils.ts

---
+++ todo-app-frontend/src/utils/dateUtils.ts
@@ -0,0 +1,8 @@
+export const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`;
+}
