0a32ea4a54d92e28198413e0ffbac9745960b25e
Leo Lobis <leo.lobis02@gmail.com>
2024-08-28 21:35:04 +0200

    Update App.tsx - Refactor state-management

--- todo-app-frontend/src/App.tsx
+++ todo-app-frontend/src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './components/TodoList/TodoList';
 import styles from './app.module.scss'
+import AppHeader from "./components/AppHeader/AppHeader";
+import {AppDispatch, RootState} from "./store/store";
+import {useDispatch, useSelector} from "react-redux";
+import {getTodos, removeTodo} from "./slices/todoSlice";
 
 const App: React.FC = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const todoState = useSelector((state: RootState) => state.todo);
+
+    useEffect(() => {
+        dispatch(getTodos());
+    }, [dispatch]);
+
+    const handleDelete = (id: number) => {
+        dispatch(removeTodo(id));
+    };
+
     return (
         <div>
             <h1 style={{ textAlign: 'center'}}>TODO LIST</h1>
-            <div className={styles.todoItemContainer}>
-                <TodoList />
+
+            <div className={styles.appContentWrapper}>
+                <AppHeader />
+                <div className={styles.todoListContainer}>
+                    <TodoList
+                        todoState={todoState}
+                        onDelete={handleDelete}
+                    />
+                </div>
             </div>
         </div>
     );
