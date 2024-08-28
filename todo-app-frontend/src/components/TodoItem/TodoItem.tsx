ad5aacae110eb78f3b913cd05ef8778bdae2e47f
Leo Lobis <leo.lobis02@gmail.com>
2024-08-28 21:36:42 +0200

    Update TodoItem.tsx - Add logic for editing and adding todos

--- todo-app-frontend/src/components/TodoItem/TodoItem.tsx
+++ todo-app-frontend/src/components/TodoItem/TodoItem.tsx
@@ -1,34 +1,103 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Todo } from '../../types/types';
 import styles from './todoItem.module.scss';
+import {formatDate} from "../../utils/dateUtils";
+import {decodePriority, decodeStatus} from "../../utils/enumUtils";
+import {AppDispatch} from "../../store/store";
+import {useDispatch} from "react-redux";
+import {addTodo, editTodo, NavigationState, setNavigationState} from "../../slices/todoSlice";
 
 interface TodoItemProps {
     todo: Todo;
+    navigation: NavigationState;
     onDelete: (id: number) => void;
-    onEdit: (todo: Todo) => void;
+    isNew: boolean;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete, onEdit }) => {
-    return (
-        <div>
-            <div className={styles.todoItem}>
-                <p className={styles.title}>{todo.title}</p>
-                <div className={styles.gridContainer}>
-                    <p>Creation Date: {todo.creationDate}</p>
-                    <p>Due Date: {todo.dueDate}</p>
-                    <p>Priority: {todo.priority}</p>
-                    <p>Status: {todo.status}</p>
-                </div>
-                {/* <section>{todo.description}</section> */}
-                {/* <p>Category: {todo.category}</p> */}
-                {/* <p>completion Date: {todo.completionDate}</p> */}
-                {/* <p>Tags: {todo.tags}</p> */}
+const TodoItem: React.FC<TodoItemProps> = ({ todo, navigation, onDelete, isNew = false }) => {
+    const dispatch: AppDispatch = useDispatch();
+    const [newTodo, setNewTodo] = useState<Todo>(todo);
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
 
-                <div className={styles.buttonContainer}>
-                    <button onClick={() => onEdit(todo)}>Edit</button>
-                    <button onClick={() => onDelete(todo.id)}>Delete</button>
-                </div>
-            </div>
+    const handleEditButtonClick = (id: number) => {
+        dispatch(setNavigationState(NavigationState.EDIT));
+        setSelectedItemId(id);
+    };
+
+    const handleCancelButtonClick = () => {
+        dispatch(setNavigationState(NavigationState.DEFAULT));
+        resetSelectedItemId();
+    };
+
+    const isCreate = (navigation: NavigationState): boolean => {
+        return navigation === NavigationState.CREATE;
+    };
+
+    const isEdit = (navigation: NavigationState): boolean => {
+        return navigation === NavigationState.EDIT;
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setNewTodo(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleSave = () => {
+        if (isCreate(navigation)) {
+            dispatch(addTodo(newTodo));
+        } else if (isEdit(navigation)) {
+            dispatch(editTodo({id: todo.id, todo: newTodo}));
+        }
+        dispatch(setNavigationState(NavigationState.DEFAULT));
+        resetSelectedItemId();
+    };
+
+    const resetSelectedItemId = () => {
+        setSelectedItemId(null);
+    };
+
+    return (
+        <div className={styles.todoItem}>
+            { (isCreate(navigation) && isNew) || (isEdit(navigation) && selectedItemId === todo.id) ? (
+                <>
+                    <input
+                        name="title"
+                        placeholder="Title"
+                        value={newTodo.title}
+                        onChange={handleInputChange}
+                    />
+                    <textarea
+                        name="description"
+                        placeholder="Description"
+                        value={newTodo.description}
+                        onChange={handleInputChange}
+                    />
+                    <div className={styles.buttonContainer}>
+                        <button onClick={handleSave}>Save</button>
+                        <button onClick={handleCancelButtonClick}>Cancel</button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <p className={styles.title}>{todo.title}</p>
+                    <section>{todo.description}</section>
+                    <div className={styles.gridContainer}>
+                        <p>Creation Date: {formatDate(todo.creationDate)}</p>
+                        <p>Due Date: {formatDate(todo.dueDate)}</p>
+                        <p>Priority: {decodePriority(todo.priority)}</p>
+                        <p>Status: {decodeStatus(todo.status)}</p>
+                    </div>
+                    {/* <p>Category: {todo.category}</p> */}
+                    {/* <p>completion Date: {todo.completionDate}</p> */}
+                    {/* <p>Tags: {todo.tags}</p> */}
+
+                    <div className={styles.buttonContainer}>
+                        <button onClick={() => handleEditButtonClick(todo.id)}>Edit</button>
+                        <button onClick={() => onDelete(todo.id)}>Delete</button>
+                    </div>
+                </>
+            )}
+
         </div>
     );
 };
