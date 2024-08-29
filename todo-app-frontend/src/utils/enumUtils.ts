1d678cd636e78f507d34c225f1e33e215a4d84a9
Leo Lobis <leo.lobis02@gmail.com>
2024-08-29 15:28:31 +0200

    Add decodeMonth method

--- todo-app-frontend/src/utils/enumUtils.ts
+++ todo-app-frontend/src/utils/enumUtils.ts
@@ -1,4 +1,4 @@
-import { Status, Priority } from "../types/types";
+import {Status, Priority, Month} from "../types/types";
 
 export const decodeStatus = (status: Status): string => {
     switch (status) {
@@ -23,5 +23,36 @@ export const decodePriority = (priority: Priority): string => {
             return 'Low';
         default:
             return 'Unknown Priority';
+    }
+}
+
+export const decodeMonth = (month: Month): string => {
+    switch (month) {
+        case Month.JANUARY:
+            return 'January';
+        case Month.FEBRUARY:
+            return 'February';
+        case Month.MARCH:
+            return 'March';
+        case Month.APRIL:
+            return 'April';
+        case Month.MAY:
+            return 'May';
+        case Month.JUNE:
+            return 'June';
+        case Month.JULY:
+            return 'July';
+        case Month.AUGUST:
+            return 'August';
+        case Month.SEPTEMBER:
+            return 'September';
+        case Month.OCTOBER:
+            return 'October';
+        case Month.NOVEMBER:
+            return 'November';
+        case Month.DECEMBER:
+            return 'December';
+        default:
+            return 'Invalid month';
     }
 }
