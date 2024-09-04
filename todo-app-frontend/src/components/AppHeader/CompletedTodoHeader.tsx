2db684bbe80ee2106a9725466accbb3f8d4ab9f0
Leo Lobis <leo.lobis02@gmail.com>
2024-09-04 19:50:54 +0200

    Update state handling in CompletedTodoHeader.tsx

--- todo-app-frontend/src/components/AppHeader/CompletedTodoHeader.tsx
+++ todo-app-frontend/src/components/AppHeader/CompletedTodoHeader.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from './appHeader.module.scss';
+import {setCompletedTaskExpanded} from "../../slices/todoSlice";
+import {AppDispatch, RootState} from "../../store/store";
+import {useDispatch, useSelector} from "react-redux";
 
 const CompletedAppHeader: React.FC = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const isCompletedTaskContainerExpanded = useSelector((state: RootState) => state.todo.isCompletedTaskContainerExpanded);
+
+    const updateExpandedState = () => {
+        dispatch(setCompletedTaskExpanded(!isCompletedTaskContainerExpanded));
+    }
+
     return (
         <div className={styles.headerContainer} style={{ marginTop: '1rem' }}>
-            <h1>Completed Tasks</h1>
+            <h1 style={{ cursor: 'pointer' }} onClick={updateExpandedState}>Completed Tasks</h1>
         </div>
     );
 };
 
-export default CompletedAppHeader;
+export default React.memo(CompletedAppHeader);
